d5608d09f8249da33b37c00b0d062a606685959a
sAbbath <52366493+cgonzalezh17@users.noreply.github.com>
2022-11-20 13:14:17 -0500

    Update Post.js

--- models/Post.js
+++ models/Post.js
@@ -16,7 +16,7 @@ const PostSchema = new mongoose.Schema(
       required: false,
     },
     username: {
-      type: Schema.Types.ObjectId,
+      type: mongoose.Schema.Types.ObjectId,
       ref: 'User',
       required: true,
     },
